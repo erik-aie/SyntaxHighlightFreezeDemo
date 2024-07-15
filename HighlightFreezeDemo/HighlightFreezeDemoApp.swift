0f95b34e5b0d1ee065705114583443ecfc337a8e
Erik Abramczyk <esda98@gmail.com>
2024-07-15 08:57:40 -0400

    Initial Commit

---
+++ HighlightFreezeDemo/HighlightFreezeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighlightFreezeDemoApp.swift
+//  HighlightFreezeDemo
+//
+//  Created by Erik Abramczyk on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HighlightFreezeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
